b1286823656b658c4ef00a6d0042e1644c52d7ec
Goran Josic <goran.josic@usi.ch>
2012-05-29 15:28:14 +0200

    Class inclusion and tabulation fixed

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 Ext.application({
-    models: ["Settings"],
 
-    name: 'MoodleMobApp',
+	name: 'MoodleMobApp',
 
-    requires: [
-        'MoodleMobApp.Config',
-        'Ext.MessageBox',
-		'Ext.data.identifier.Uuid'
-    ],
-	
-    views: [
-		'MoodleMobApp.view.Main',
-        'MoodleMobApp.view.account.Aai',
-        'MoodleMobApp.view.account.Manual',
-        'MoodleMobApp.view.account.Choice'
+	requires: [
+		'MoodleMobApp.Config',
+		'Ext.data.identifier.Uuid',
+		'MoodleMobApp.store.account.HomeOrgs',
 	],
 
-    controllers: [
+	models: ["MoodleMobApp.model.Settings"],
+
+	stores: [
+		'MoodleMobApp.store.Settings',
+		'MoodleMobApp.store.account.HomeOrgs',
+	],
+	
+	views: [
+		'MoodleMobApp.view.Main',
+		'MoodleMobApp.view.account.Aai',
+		'MoodleMobApp.view.account.Manual',
+		'MoodleMobApp.view.account.Choice'
+	],
+
+	controllers: [
 		"MoodleMobApp.controller.Main",
 		"MoodleMobApp.controller.account.Choice", 
 		"MoodleMobApp.controller.UsageAgreement", 
@@ -25,46 +31,46 @@ Ext.application({
 		'MoodleMobApp.controller.account.Manual'
 		],
 
-    icon: {
-        57: 'resources/icons/Icon.png',
-        72: 'resources/icons/Icon~ipad.png',
-        114: 'resources/icons/Icon@2x.png',
-        144: 'resources/icons/Icon~ipad@2x.png'
-    },
+	icon: {
+		57: 'resources/icons/Icon.png',
+		72: 'resources/icons/Icon~ipad.png',
+		114: 'resources/icons/Icon@2x.png',
+		144: 'resources/icons/Icon~ipad@2x.png'
+	},
 
-    phoneStartupScreen: 'resources/loading/Homescreen.jpg',
-    tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
+	phoneStartupScreen: 'resources/loading/Homescreen.jpg',
+	tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
 
-    launch: function() {
-        // Destroy the #appLoadingIndicator element
-        Ext.fly('appLoadingIndicator').destroy();
+	launch: function() {
+		// Destroy the #appLoadingIndicator element
+		Ext.fly('appLoadingIndicator').destroy();
 		var settings_store = Ext.create('MoodleMobApp.store.Settings');
 		settings_store.load();
 
 		if( settings_store.first().getData().usageagreement == false ) {
-        	Ext.Viewport.add( Ext.create('MoodleMobApp.view.UsageAgreement') );
+			Ext.Viewport.add( Ext.create('MoodleMobApp.view.UsageAgreement') );
 		} else if( settings_store.first().getData().accounttype == '' ) {
-        	Ext.Viewport.add(Ext.create('MoodleMobApp.view.account.Choice'));
+			Ext.Viewport.add(Ext.create('MoodleMobApp.view.account.Choice'));
 		} else {
-        	Ext.Viewport.add(Ext.create('MoodleMobApp.view.Main'));
+			Ext.Viewport.add(Ext.create('MoodleMobApp.view.Main'));
 		}
 
-        // Initialize the main view
+		// Initialize the main view
 		//var aai_account_form = Ext.create('MoodleMobApp.view.account.Aai');
-        //Ext.Viewport.add( aai_account_form );
+		//Ext.Viewport.add( aai_account_form );
 		//var manual_account_form = Ext.create('MoodleMobApp.view.account.Manual');
-        //Ext.Viewport.add( manual_account_form );
-    },
+		//Ext.Viewport.add( manual_account_form );
+	},
 
-    onUpdated: function() {
-        Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
-    }
+	onUpdated: function() {
+		Ext.Msg.confirm(
+			"Application Update",
+			"This application has just successfully been updated to the latest version. Reload now?",
+			function(buttonId) {
+				if (buttonId === 'yes') {
+					window.location.reload();
+				}
+			}
+		);
+	}
 });

87e708643679ec15e2ff3ddff69d3ddab781d98d
Goran Josic <goran.josic@usi.ch>
2012-08-09 15:46:04 +0200

    Moved the course module count status code to Main
    Moved the course module count status code from CourseNavigator
    to Main. The Main controller is going to be used for caching and
    status updating functionalities.

--- app/controller/CourseNavigator.js
+++ app/controller/CourseNavigator.js
@@ -23,44 +23,6 @@ Ext.define('MoodleMobApp.controller.CourseNavigator', {
 		}
 	},
 	
-	init: function(){
-		var courses_store = Ext.data.StoreManager.lookup('courses');
-		// set listener for updating the course module stats
-		courses_store.on('write', this.updateCourseStats, this, {single:true});
-	},
-
-	updateCourseStats: function() {
-		// 'this' points to the courses_store
-		// collect course ids
-		var courses_store = Ext.data.StoreManager.lookup('courses');
-		var course_ids = new Array();
-		courses_store.each(function(entry) {
-			course_ids.push(entry.getData().id);
-		});
-		// set the token for this request
-		// any token will do, in this case the token
-		// of the first course entry is used
-		MoodleMobApp.Session.setCourseToken(courses_store.first().getData().token);
-		// request the course modules count
-		console.log('loading');
-		var course_modules_count_store = MoodleMobApp.WebService.getCourseModulesCount(course_ids);
-		course_modules_count_store.on(
-				'load', 
-				function(){
-					console.log('loaded');
-					course_modules_count_store.each(function(entry){
-						var courses_store_entry = courses_store.getById(entry.getData().id);
-						courses_store_entry.getData().modulestatus = entry.getData().modulescount + ' modules';
-						courses_store_entry.getData().modules = entry.getData().modulescount;
-						courses_store_entry.setDirty();
-					});
-					courses_store.sync();
-				},
-				this,
-				{single: true}
-		);
-
-	},
 
 	selectCourse: function(view, index, target, record) {
 		var course_data = record.getData();

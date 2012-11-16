35d379ddd3becb163c1609e51c1680388d943447
Goran Josic <goran.josic@usi.ch>
2012-11-16 15:38:39 +0100

    Config file changed but was --assume-unchanged
    The Config.js has been changed but the
    
    	git update-index --assume-unchanged app/Config.js
    
    command prevented commits.
    
    	git update-index --no-assume-unchanged app/Config.js
    
    made it available again. So committing differences here.

--- app/Config.js
+++ app/Config.js
@@ -4,6 +4,7 @@ Ext.define('MoodleMobApp.Config', {
 	constructor: function(config) {
 		// DRY config approach
 		this.config.aaiAuthUrl = this.config.moodleUrl + this.config.aaiAuthUrl;
+		this.config.homeOrgsUrl = this.config.moodleUrl + this.config.homeOrgsUrl;
 		this.config.manualAuthUrl = this.config.moodleUrl + this.config.manualAuthUrl;
 		this.config.webServiceUrl = this.config.moodleUrl + this.config.webServiceUrl;
 
@@ -14,8 +15,8 @@ Ext.define('MoodleMobApp.Config', {
 	config : {
 		moodleUrl : 'https://www.yourmoodleinstallation.ch',
 		aaiAuthUrl : '/auth/mobileaai/authenticate.php',
+		homeOrgsUrl : '/auth/mobileaai/get_idps.php',
 		manualAuthUrl : '/auth/mobilemanual/authenticate.php',
 		webServiceUrl : '/webservice/rest/server.php',
 	}
-	
 });

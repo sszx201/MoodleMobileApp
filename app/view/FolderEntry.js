60b8e840cafe850fa37de3d4af9ac4aed35dad58
Goran Josic <goran.josic@usi.ch>
2014-02-21 10:50:42 +0100

    Folder Entries glitch fix
    During the navigation in the folder structures the icons were
    sometimes mismatched. The folder entries would get the file icon
    and viceversa. This commit fixes the issue.

--- app/view/FolderEntry.js
+++ app/view/FolderEntry.js
@@ -27,6 +27,13 @@ Ext.define("MoodleMobApp.view.FolderEntry", {
 		this.down('#name').setHtml(record.get('name'));
 		this.down('#mime').setHtml(record.get('mime'));
 
+		// remove previous css classes if any
+		// this is the folder navigation icons glitch fix
+		// without this sometimes the folder entries get
+		// the file icon and viceversa
+		this.removeCls('x-subfolder-icon');
+		this.removeCls('x-file-icon');
+		// add the correct css class
 		if(record.get('mime') == 'inode/directory'){
 			this.addCls('x-subfolder-icon');
 		} else {
